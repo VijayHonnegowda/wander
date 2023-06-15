94b177d07dd16818886ddde053c87169a90c3db9
VijayHonnegowda <127085102+VijayHonnegowda@users.noreply.github.com>
2023-06-15 09:46:42 +0530

    added the package in app export file.

--- lib/constants/app_exports.dart
+++ lib/constants/app_exports.dart
@@ -1,4 +1,12 @@
+export 'package:uuid/uuid.dart';
+export 'package:wander/map.dart';
 export 'package:wander/location.dart';
 export 'package:flutter/material.dart';
 export 'package:wander/home_screen.dart';
-export 'package:location/location.dart';
+export 'package:geocoding/geocoding.dart';
+export 'package:geolocator/geolocator.dart';
+export 'package:wander/polygone_screen.dart';
+export 'package:wander/current_location.dart';
+export 'package:wander/google_places_api.dart';
+export 'package:wander/custom_marker_info.dart';
+export 'package:google_maps_flutter/google_maps_flutter.dart';
