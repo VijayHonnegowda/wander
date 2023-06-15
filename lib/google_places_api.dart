73a7ee019d24c01fa5529d6049c51787a74e876b
VijayHonnegowda <127085102+VijayHonnegowda@users.noreply.github.com>
2023-06-15 09:53:44 +0530

    create google places api file and added Places api.

---
+++ lib/google_places_api.dart
@@ -0,0 +1,49 @@
+import 'package:google_places_flutter/model/prediction.dart';
+import 'package:wander/constants/app_exports.dart';
+
+class GooglePlacesApiScreen extends StatefulWidget {
+  const GooglePlacesApiScreen({super.key});
+
+  @override
+  State<GooglePlacesApiScreen> createState() => _GooglePlacesApiScreenState();
+}
+
+class _GooglePlacesApiScreenState extends State<GooglePlacesApiScreen> {
+  // String googleApiKey = "AIzaSyBcgsqJ9BMHSLZheEeB4-A9V9du4fEaKgo" ;
+
+  void showGoogleAutoComplete() async {
+   const kGoogleApiKey = "AIzaSyBcgsqJ9BMHSLZheEeB4-A9V9du4fEaKgo";
+
+   var Mode;
+   var PlacesAutocomplete;
+   Prediction p = await PlacesAutocomplete.show(
+     context:context,
+     apiKey:kGoogleApiKey,
+     mode: Mode.overlay,
+   );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Google Places API"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                labelText: "Search"
+              ),
+              onTap: (){
+                showGoogleAutoComplete();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
