ada7726785efb338dd3dbf90a5bcaf53dd41366a
VijayHonnegowda <127085102+VijayHonnegowda@users.noreply.github.com>
2023-06-16 11:44:29 +0530

    create app color file.

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primaryColor = Colors.black;
+  static Color secondaryColor = Colors.greenAccent.withOpacity(0.4);
+}
